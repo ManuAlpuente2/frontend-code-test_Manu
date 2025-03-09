05272697e19f1edf54edc0ff9dd6cc4eac46b352
Manu Alpuente <malpuente@smartvel.com>
2025-03-09 23:08:58 +0100

    Build

---
+++ build/precache-manifest.64f7074512eafdb76261a5228572276c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d71e75e8a8c3181ebf5def838f1cb283",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f286403aad32f6216816",
+    "url": "./static/css/main.c99046e4.chunk.css"
+  },
+  {
+    "revision": "ba59e326cdc0a38092f5",
+    "url": "./static/js/2.f1a98bd2.chunk.js"
+  },
+  {
+    "revision": "de1cedeacf4641d6277b42334cfacb1d",
+    "url": "./static/js/2.f1a98bd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f286403aad32f6216816",
+    "url": "./static/js/main.080dd7b0.chunk.js"
+  },
+  {
+    "revision": "53a3b6149d566e2599e5",
+    "url": "./static/js/runtime-main.fb3194f4.js"
+  },
+  {
+    "revision": "f3fa54cb704f1f0c29867fead6c3d1fb",
+    "url": "./static/media/add.f3fa54cb.svg"
+  },
+  {
+    "revision": "da7c140d7c43775668edb5acdb59c44a",
+    "url": "./static/media/color.da7c140d.svg"
+  },
+  {
+    "revision": "99d05255a0c2840c3c7bbaa1a179ca95",
+    "url": "./static/media/redo.99d05255.svg"
+  },
+  {
+    "revision": "526be17898d6bee56347c824abc923e5",
+    "url": "./static/media/remove.526be178.svg"
+  },
+  {
+    "revision": "477a75f8d30f9f1f03d6111ce3302558",
+    "url": "./static/media/undo.477a75f8.svg"
+  }
+]);
